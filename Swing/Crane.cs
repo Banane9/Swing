f23f0fca74e84c36200188bb071d8497db04d0f5
Arne Kiesewetter <banane1996@gmx.net>
2015-05-02 23:24:10 +0200

    Add playing field and basic crane.

---
+++ Swing/Crane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swing
+{
+    /// <summary>
+    /// Represents the crane that picks <see cref="Ball"/>s from the <see cref="BallReservoir"/> and drops them onto the Seesaws.
+    /// </summary>
+    public sealed class Crane
+    {
+        public Ball CurrentBall { get; private set; }
+    }
+}
---
+++ Swing/PlayingField.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swing
+{
+    /// <summary>
+    /// Represents the parts of the playing field.
+    /// </summary>
+    public sealed class PlayingField
+    {
+        /// <summary>
+        /// Gets the <see cref="BallReservoir"/> that the <see cref="Crane"/> takes the <see cref="Ball"/>s from.
+        /// </summary>
+        public BallReservoir BallReservoir { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="Crane"/> that picks <see cref="Ball"/>s from the <see cref="BallReservoir"/> and drops them onto the Seesaws.
+        /// </summary>
+        public Crane Crane { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="Ball"/>s on the Seesaws.
+        /// </summary>
+        public Ball[][] Seesaws { get; private set; }
+    }
+}
